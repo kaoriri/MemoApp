884a82dcfa88f52d1e1b7d78b95ad2f8d5bf3627
hiramatsukaori@gmail.com <kaori.hiramatsu@gmail.com>
2020-04-06 13:52:37 +0900

    Create MemoList

---
+++ Development/udemy/MemoApp/src/elements/BodyText.js
@@ -0,0 +1,22 @@
+import { StyleSheet, View, Text } from 'react-native';
+import React from 'react';
+
+class BodyText extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text style={styles.text}>
+          {this.props}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#DDD',
+    backgroundColor: '#eee',
+  },
+});
+export default BodyText;
